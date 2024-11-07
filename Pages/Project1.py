79e992438d9516ceccaa94204eeaf50a7f1bf0ac
oiyoi420 <klimashenko.2017@gmail.com>
2024-11-07 18:05:50 +0500

    fgf

--- Pages/Project1.py
+++ Pages/Project1.py
@@ -1,7 +1,37 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Декоратор для кэширования
 
 class Project1:
     def __init__(self):
         pass
+
     def app(self):
-        st.write('Project1')
+        st.title('Creation of DataFrame')
+
+    def load_data(file):
+        if file is not None:
+            data = pd.read_csv(file)
+            return data
+        return None
+
+upload = st.file_uploader("choose csv file")
+if upload is not None:
+    df = load_data(upload)
+    st.dataframe(df, height=400, width=600)
+else:
+    st.warning("Please upload a CSV file")
+
+st.markdown(
+    """
+    <style>
+    h1 {
+        color: green;
+        font-size:18px;
+        text-align:center;
+    }
+    </style>
+    """, unsafe_allow_html=True
+)
