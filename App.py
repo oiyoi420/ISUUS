ddb7fa4b8be880d82ac3b5be04f03bc4e68ed230
oiyoi420 <klimashenko.2017@gmail.com>
2024-11-07 17:42:48 +0500

    fgf

--- App.py
+++ App.py
@@ -1,29 +1,42 @@
 import streamlit as st
 from Pages import Home, Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar as navbar
+from PIL import Image
+import pandas as pd
+import numpy as np
 
-st.set_page_config(initial_sidebar_state="collapsed")
+image = Image.open('img/images.jpg')
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
+
 pages = ['Home', 'Project1', 'Project2', 'Project3']
 styles = {
-    "nav": {
-        "background-color": "rgb(123, 209, 146)",
-    },
-    "div": {
-        "max-width": "32rem",
-    },
-    "span": {
-        "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
-        "padding": "0.4375rem 0.625rem",
-    },
-    "active": {
-        "background-color": "rgba(105, 114, 255, 0.25)",
-    },
-    "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
-    }
+  "nav": {
+    "background-color": "royalblue",
+    "display": "flex",
+    "justify-content": "center"
+  },
+  "img": {
+    "position": "absolute",
+    "left": "-20px",
+    "font-size": "15px",
+    "top": "4px",
+    "width": "100px",
+    "height": "40px"
+  },
+  "span": {
+    "display": "block",
+    "color": "white",
+    "padding": "0.2rem 0.725rem",
+    "font-size": "14px"
+  },
+  "active": {
+    "background-color": "white",
+    "color": "black",
+    "font-weight": "normal",
+    "padding": "14px"
+  }
 }
+
 
 page = navbar(pages, styles=styles)
 
